186aae3cc169cb1563d3b8b1d030555413365733
Warun Sharma <warunsharma.95@gmail.com>
2025-04-19 15:44:35 +0530

    [user-service] feat: health api

--- services/user-service/src/routes/health.route.ts
+++ services/user-service/src/routes/health.route.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import {Request, Response, Router} from 'express';
 
 const router = Router();
 
-router.get('/health-check', (req, res) => {
+router.get('/health-check', (req: Request, res: Response) => {
     res.status(200).json({status: 'Up', timestamp: new Date().toISOString()});
-})
+});
 
 export default router;
