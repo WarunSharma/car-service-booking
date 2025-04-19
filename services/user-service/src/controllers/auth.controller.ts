41dbba19e07410a5c60c20f5cc82d9c56956dfdb
Warun Sharma <warunsharma.95@gmail.com>
2025-04-19 06:52:39 +0530

    [user-service] feat: profile route

--- services/user-service/src/controllers/auth.controller.ts
+++ services/user-service/src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { AuthRequest } from '../middlewares/auth.middleware';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -6,64 +7,84 @@ import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecret';
 
-export const register = async(req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
     try {
-        const {name, email, password} = req.body;
-        const existingUser = await prisma.user.findUnique({where: {
-            email
-        }});
+        const { name, email, password } = req.body;
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
 
         if (existingUser) {
-            res.status(400).json({error: 'User already exists'});
+            res.status(400).json({ error: 'User already exists' });
         }
         else {
             const hashedPassword = await bcrypt.hash(password, 10);
             const newUser = await prisma.user.create({
 
-                data: {name, email, password: hashedPassword},
+                data: { name, email, password: hashedPassword },
             })
 
-            res.status(201).json({id: newUser.id, name: newUser.name, email: newUser.email});
+            res.status(201).json({ id: newUser.id, name: newUser.name, email: newUser.email });
         }
     } catch (error) {
-        res.status(500).json({error: 'Server error'});
+        res.status(500).json({ error: 'Server error' });
     }
 }
 
-export const login = async(req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
     try {
-        const {email, password} = req.body;
-        const user = await prisma.user.findUnique({where: {email}});
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { email } });
 
         if (!user) {
-            res.status(401).json({error: 'Invalid credentials'});
+            res.status(401).json({ error: 'Invalid credentials' });
         }
         else {
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
-                res.status(401).json({error: 'Invalid credentials'});
+                res.status(401).json({ error: 'Invalid credentials' });
             }
             else {
-                const token = jwt.sign({id: user.id, email: user.email}, JWT_SECRET, {expiresIn: '1h'});
-                res.json({token});
+                const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
+                res.json({ token });
             }
         }
     } catch (error) {
-        res.status(500).json({error: 'Server error'});
+        res.status(500).json({ error: 'Server error' });
     }
 }
 
-export const verifyToken = async(req: Request, res: Response) => {
+export const verifyToken = async (req: Request, res: Response) => {
     try {
         const token = req.headers?.authorization?.split(' ')[1];
         if (!token) {
-            res.status(401).json({error: 'Token missing'});
+            res.status(401).json({ error: 'Token missing' });
         }
         else {
             const decoded = await jwt.verify(token, JWT_SECRET);
-            res.json({valid: true, decoded})
+            res.json({ valid: true, decoded })
         }
     } catch (error) {
-        res.status(401).json({error: 'Invalid error'});
+        res.status(401).json({ error: 'Invalid error' });
+    }
+}
+
+export const getProfile = async (req: AuthRequest, res: Response) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: req.user.email },
+            select: { id: true, name: true, email: true, createdAt: true }
+        });
+
+        if (!user) {
+            res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json(user);
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Server error' });
     }
 }
--- services/user-service/src/routes/auth.routes.ts
+++ services/user-service/src/routes/auth.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import {register, login, verifyToken} from '../controllers/auth.controller';
+import { register, login, verifyToken } from '../controllers/auth.controller';
+import { getProfile } from "../controllers/auth.controller";
+import { authenticationToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/verify', verifyToken);
+router.get('/profile', authenticationToken, getProfile);
 
 export default router;
