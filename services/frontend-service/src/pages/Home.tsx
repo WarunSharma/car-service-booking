bc8983b26b5ce22832864a63067e659b00b2a0cd
Warun Sharma <warunsharma.95@gmail.com>
2025-05-02 15:16:02 +0530

    [frontend-service] feat: display user name

--- services/frontend-service/src/pages/Home.tsx
+++ services/frontend-service/src/pages/Home.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import userApi from "../services/userApi";
 
-function Home() { 
+function Home() {
+  const [user, setUser] = useState<{id: number, name: string, email: string, createdAt: string} | null>(null);
+  useEffect(() => {
+    try {
+      const fetchUser = async () => {
+        const res = await userApi.get("/api/auth/profile", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        if (res.status === 200) {
+          setUser(res.data);
+        }
+      };
+
+      fetchUser();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Welcome User</h1>
-      <button className="bg-blue-600 text-white px-4 py-2 rounded mr-4">Book Appointment</button>
-      <button className="bg-red-500 text-white px-4 py-2 rounded">Logout</button>
+      <h1 className="text-2xl font-bold mb-4">Welcome {user?.name}</h1>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded mr-4">
+        Book Appointment
+      </button>
+      <button className="bg-red-500 text-white px-4 py-2 rounded">
+        Logout
+      </button>
     </div>
   );
 }
