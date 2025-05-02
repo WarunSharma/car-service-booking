c624b8cf6d9b0f8c634a0a65b0f1f6a49656dfbb
Warun Sharma <warunsharma.95@gmail.com>
2025-05-02 13:01:09 +0530

    feat: home page

--- services/frontend-service/src/App.tsx
+++ services/frontend-service/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <Routes>
+      <Route path='/' element={<Home />}/>
       <Route path='/signup' element={<Signup />}/>
       <Route path='/login' element={<Login />}/>
     </Routes>
---
+++ services/frontend-service/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+function Home() { 
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Welcome User</h1>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded mr-4">Book Appointment</button>
+      <button className="bg-red-500 text-white px-4 py-2 rounded">Logout</button>
+    </div>
+  );
+}
+
+export default Home;
