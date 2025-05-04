806987f1f7b53b4e82f376c775b9c64ac983e1b1
Warun Sharma <warunsharma.95@gmail.com>
2025-05-04 22:19:58 +0530

    [frontend-service] Fix: Load only VITE_ environment variables in vite.config.js

--- services/frontend-service/vite.config.ts
+++ services/frontend-service/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [react()],
+    define: {
+      'import.meta.env.VITE_USER_API_URL': JSON.stringify(env.VITE_USER_API_URL),
+      'import.meta.env.VITE_APPOINTMENT_API_URL': JSON.stringify(env.VITE_APPOINTMENT_API_URL),
+      'import.meta.env.VITE_PORT': JSON.stringify(env.VITE_PORT),
+    },
     server: {
-      port: parseInt(env.VITE_PORT)
+      port: parseInt(env.VITE_PORT),
     },
   }
 })
